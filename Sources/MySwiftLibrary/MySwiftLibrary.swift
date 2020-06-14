92358cbd9643f5dc4970651aa4cf1106292144cc
Emil Atanasov <heitara@DESKTOP-KU9EJJS.localdomain>
2020-06-15 00:57:15 +0300

    extend the library

--- Sources/MySwiftLibrary/MySwiftLibrary.swift
+++ Sources/MySwiftLibrary/MySwiftLibrary.swift
@@ -1,3 +1,13 @@
-struct MySwiftLibrary {
-    var text = "Hello, World!"
+public struct MySwiftLibrary {
+   ///
+   /// Public constructor
+   /// 
+   public init(text: String = "") {
+       self.text = text
+   }
+   
+   /// Make this property public accessible
+   public var text = "Hello, World!"
+
+   public let version = "alpha-1"
 }
